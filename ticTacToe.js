75d22c75c4f611b6b8a3d0511ff1c0b7c4c9fe95
Ben Ellison <ellison.ben@gmail.com>
2016-09-30 22:32:07 +0100

    initial functions for ticTacToe

---
+++ ticTacToe.js
@@ -0,0 +1,29 @@
+function newBoard () {
+
+}
+
+function changePlayer (player) {
+    
+}
+
+function checkValidity (move, board) {
+
+}
+
+function playMove (move, board, player) {
+
+}
+
+function isWinner (player, board) {
+
+}
+
+function isFull (board) {
+
+}
+
+function playGame () {
+    var board = newBoard();
+    var player
+    
+}
