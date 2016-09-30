a16ebc38cee1bf67d1db14de1d95d52dd6b0c9d8
Ben Ellison <ellison.ben@gmail.com>
2016-09-30 23:31:57 +0100

    added isFull and isWinner functions

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,9 +1,10 @@
 function newBoard () {
-
+    var board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+    return board;
 }
 
 function changePlayer (player) {
-    
+    return player*-1;
 }
 
 function checkValidity (move, board) {
@@ -15,15 +16,38 @@ function playMove (move, board, player) {
 }
 
 function isWinner (player, board) {
-
+    for (var i=0; i<3; i++) {
+        if (board[i][0] == board[i][1] == board[i][2] == player) { 
+            return true;
+        }
+        else if (board[0][i] == board[1][i] == board[2][i] == player) {
+            return true;
+        }
+    }
+    if (board[0][0] == board[1][1] == board[2][2] == player) {
+        return true;
+    }
+    else if (board[0][2] == board[1][1] == board[2][0] == player) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 function isFull (board) {
-
+    for (var i=0; i<3; i++) {
+        for (var j=0; i<3; i++) {
+            if (board[i][j] == 0) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 function playGame () {
     var board = newBoard();
-    var player
+    var player = 1;
     
 }
