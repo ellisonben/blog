d1625f9a419573b71826433abf20f1fc487d5f66
Ben Ellison <ellison.ben@gmail.com>
2016-10-25 23:22:34 +0100

    added initial animations

---
+++ animations.js
@@ -0,0 +1,22 @@
+var hilary = document.querySelector("#hilary");
+var trump = document.querySelector("#trump");
+
+var angle = 0, antiAngle = 0, lastTime = null;
+
+function animate(time) {
+    if (lastTime != null) {
+        angle += (time - lastTime) * 0.001;
+    }
+    lastTime = time;
+    
+    hilary.style.top = (Math.sin(angle) * 70 + 100) + "px";
+    hilary.style.left = (Math.cos(angle) * 200 + 250) + "px";
+    
+    trump.style.top = (Math.sin(angle) * -70) + 100 + "px";
+    trump.style.left = (Math.cos(angle) * -200) + 250 + "px";
+    
+    requestAnimationFrame(animate);
+}
+
+requestAnimationFrame(animate);
+    
