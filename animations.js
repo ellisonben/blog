b14feb4bac059378620521e125cbcb65110a4ea2
Ben Ellison <ellison.ben@gmail.com>
2016-10-27 20:23:59 +0100

    enclose in anonymous function

--- animations.js
+++ animations.js
@@ -1,22 +1,23 @@
-var hilary = document.querySelector("#hilary");
-var trump = document.querySelector("#trump");
+(function() {
+    var hilary = document.querySelector("#hilary");
+    var trump = document.querySelector("#trump");
 
-var angle = 0, antiAngle = 0, lastTime = null;
+    var angle = 0, antiAngle = 0, lastTime = null;
 
-function animate(time) {
-    if (lastTime != null) {
-        angle += (time - lastTime) * 0.001;
+    function animate(time) {
+        if (lastTime != null) {
+            angle += (time - lastTime) * 0.001;
+        }
+        lastTime = time;
+        
+        hilary.style.top = (Math.sin(angle) * 70 + 100) + "px";
+        hilary.style.left = (Math.cos(angle) * 200 + 250) + "px";
+        
+        trump.style.top = (Math.sin(angle) * -70) + 100 + "px";
+        trump.style.left = (Math.cos(angle) * -200) + 250 + "px";
+        
+        requestAnimationFrame(animate);
     }
-    lastTime = time;
-    
-    hilary.style.top = (Math.sin(angle) * 70 + 100) + "px";
-    hilary.style.left = (Math.cos(angle) * 200 + 250) + "px";
-    
-    trump.style.top = (Math.sin(angle) * -70) + 100 + "px";
-    trump.style.left = (Math.cos(angle) * -200) + 250 + "px";
-    
-    requestAnimationFrame(animate);
-}
 
-requestAnimationFrame(animate);
-    
+    requestAnimationFrame(animate);
+})();
